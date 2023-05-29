971c7e8c448fbd3ab7a2b9cc5ec56080598c538e
Aarsyth <39145409+tobyarnett@users.noreply.github.com>
2023-05-29 12:17:39 -0500

    Updated main.py to add user prompts and darknet search functionality
    In this commit, main.py has been updated to prompt the user for input, use the input to perform a darknet search with a TorScraper instance, and save the results to a text file with a name based on the input and the current date/time.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,52 @@
-# Import necessary libraries
-from .tor_scraper import TorConnection, DarknetScraper
+####################################################################################
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░Darknet OSINT░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░Developed by Aarsyth░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░░░░░GitHub Repository: https://github.com/CyberOneTechnologies/░░░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░For support, reach out on Discord: Aarsyth#0563░░░░░░░░░░░░░░░░░░#
+####################################################################################
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#░░░░░░░░█████╗░██╗░░░██╗██████╗░███████╗██████╗░░█████╗░███╗░░██╗███████╗░░░░░░░░░#
+#░░░░░░░██╔══██╗╚██╗░██╔╝██╔══██╗██╔════╝██╔══██╗██╔══██╗████╗░██║██╔════╝░░░░░░░░░#
+#░░░░░░░██║░░╚═╝░╚████╔╝░██████╦╝█████╗░░██████╔╝██║░░██║██╔██╗██║█████╗░░░░░░░░░░░#
+#░░░░░░░██║░░██╗░░╚██╔╝░░██╔══██╗██╔══╝░░██╔══██╗██║░░██║██║╚████║██╔══╝░░░░░░░░░░░#
+#░░░░░░░╚█████╔╝░░░██║░░░██████╦╝███████╗██║░░██║╚█████╔╝██║░╚███║███████╗░░░░░░░░░#
+#░░░░░░░░╚════╝░░░░╚═╝░░░╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝╚══════╝░░░░░░░░░#
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+####################################################################################
 
-# Main function
+import datetime
+import os
+from app.scrapers.tor_scraper import TorScraper
+
 def main():
-    tor_connection = TorConnection()
-    darknet_scraper = DarknetScraper()
+    # Prompt user for input
+    first_name = input("First name: ")
+    last_name = input("Last name: ")
+    phone_number = input("Phone number (numbers only): ")
+    country = input("Country: ")
+    email_address = input("Email address: ")
+    address = input("Address: ")
+    city = input("City: ")
+    state = input("State: ")
+    zip_code = input("ZIP code: ")
 
-    tor_connection.start_tor()
-    # Perform your search here
-    tor_connection.stop_tor()
+    # Create a TorScraper instance
+    tor_scraper = TorScraper()
+
+    # Perform darknet search
+    results = tor_scraper.search(first_name, last_name, phone_number, country, email_address, address, city, state, zip_code)
+
+    # Get current date/time
+    now = datetime.datetime.now()
+
+    # Generate output file name
+    filename = f"darknetquery.{first_name}.{last_name}.{now.strftime('%Y%m%d%H%M%S')}.txt"
+
+    # Save results to output file
+    with open(filename, "w") as file:
+        file.write(results)
+
+    print(f"Results saved to {filename}")
 
 if __name__ == "__main__":
     main()
